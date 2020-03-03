a347909ab567dcb9d0af2f00f2c0baf5d8eb2478
Jamison Cozart <jamisoncozart@gmail.com>
2020-03-02 19:30:46 -0800

    Test for GetDescription() method, pass test

--- ToDoList.Tests/ModelTests/ItemTests.cs
+++ ToDoList.Tests/ModelTests/ItemTests.cs
@@ -9,8 +9,22 @@ namespace ToDoList.Tests
     [TestMethod]
     public void ItemConstructor_CreatesInstanceOfItem_Item()
     {
-      Item newItem = new Item();
+      Item newItem = new Item(null);
       Assert.AreEqual(typeof(Item), newItem.GetType());
+    }
+
+    [TestMethod]
+    public void GetDescription_ReturnsDescription_String()
+    {
+      //arrange
+      string description = "Walk the dog.";
+      Item newItem = new Item(description);
+      
+      //act
+      string result = newItem.Description;
+      
+      //assert
+      Assert.AreEqual(description, result);
     }
   }
 }
--- ToDoList/Models/Item.cs
+++ ToDoList/Models/Item.cs
@@ -2,6 +2,10 @@ namespace ToDoList.Models
 {
   public class Item
   {
-    //properties + methods
+    public string Description {get;}
+    public Item(string description)
+    {
+      Description = description;
+    }
   }
 }
